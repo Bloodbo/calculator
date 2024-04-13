21a273fe94f83bc865c825041ba5672669a1c01f
Bloodbo <133759306+Bloodbo@users.noreply.github.com>
2024-04-13 12:06:50 +0300

    Add files via upload

---
+++ calculator.h
@@ -0,0 +1,8 @@
+#pragma once
+class calculator
+{
+public:
+	double Calculate(double x, char oper, double y);
+};
+/*Этот код объявляет новую функцию под названием Calculate, 
+которую мы используем для выполнения математических операций сложения, вычитания, умножения и деления.*/
---
+++ calculator_just.cpp
@@ -0,0 +1,33 @@
+﻿#include <iostream>
+#include "Calculator.h"
+using namespace std;
+
+
+int main()
+{
+    setlocale(0, "");
+    double x = 0.0;
+    double y = 0.0;
+    double result = 0.0;
+    char oper = '+';
+    cout << "это калькулятор!" << endl;
+    cout << "Пожалуйста, введите операцию, которую необходимо выполнить. Формат ввода: a+b | a-b | a*b | a/b" << endl;
+
+    calculator c;
+    while (true)
+    {
+        cin >> x >> oper >> y;
+        if (oper == '/' && y == 0)
+        {
+            cout << "Попытался разделить на ноль!" << endl;
+            continue;
+        }
+        else
+        {
+            result = c.Calculate(x, oper, y);
+        }
+        cout << "Результат " << "из " << x << oper << y << " это: " << result << endl;
+    
+    }
+}
+
